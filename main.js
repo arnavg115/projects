e84f559a05ab61c340a4d4e33eaf26dab58dc4b6
Arnav G <pyeushgurha@pyeushs-imac.attlocal.net>
2021-01-07 11:30:46 -0800

    more js

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 function func(){
 	setTimeout(function(){
+		var nav = document.getElementById("nav1");
+		nav.style.opacity = "1"
+		nav.style.width = "90vw"
+	},500);
+	
+	setTimeout(function(){
 		var id1 = document.getElementById("1");
 		id1.style.opacity = "1";
-	},500);
+	},1000);
 	setTimeout(function() {
   		var id2 = document.getElementById("2");
   		id2.style.opacity = "1"
-	}, 1000);
+	}, 1500);
 	setTimeout(function() {
   		var id3 = document.getElementById("3");
   		id3.style.opacity = "1"
-	}, 1500);
+	}, 2000);
 }
